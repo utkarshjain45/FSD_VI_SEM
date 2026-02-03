0aedc2968e4107cf87f65b76df7b67b7f41efc88
Utkarsh Jain <jainutkarsh096@gmail.com>
2026-02-03 11:49:10 +0530

    Read File by fs using callback

--- fs-system/index.js
+++ fs-system/index.js
@@ -1,5 +1,15 @@
 const fs = require("fs");
 
 // fs.writeFileSync("./abes.txt", "Hii ABES College!!")
-const res = fs.readFileSync("./abes.txt", "utf-8")
-console.log(res)
+// const res = fs.readFileSync("./abes.txt", "utf-8")
+// console.log(res)
+
+// fs.writeFile("./abes.txt", "We are Coding", () => {})
+fs.readFile("./abes.txt", "utf-8", (err, result) => {
+    if(err){
+        console.log("Error:", err);
+    }
+    else{
+        console.log(result);
+    }
+});
